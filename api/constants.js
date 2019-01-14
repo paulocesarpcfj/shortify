d84fad3a2965698a63ae37c135773b96c0d908e2
Paulo Ferreira <Paulo.Ferreira@saiglobal.com>
2019-01-14 20:00:02 -0200

    Set default value for mongoURI

--- api/constants.js
+++ api/constants.js
@@ -1,5 +1,5 @@
 export default {
-    mongoURI: 'mongodb://127.0.0.1:27017/mongodb',
+    mongoURI: 'mongodb://mongodb:27017/mongodb',
     errorUrl: 'http://localhost:3000',
     baseUrl: 'http://localhost:7000'
 };
